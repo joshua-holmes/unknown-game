5e56b85994b761d73e74793a3d0717d25c05664d
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-01-17 17:39:55 -0800

    Setup graphics device

--- src/window_setup/vulkan/mod.rs
+++ src/window_setup/vulkan/mod.rs
@@ -4,15 +4,16 @@ use vulkano::{
     instance::{Instance, InstanceCreateInfo},
     swapchain::Surface,
     Version, VulkanLibrary,
+    device::{DeviceExtensions, QueueFlags, Queue, physical::{PhysicalDevice, PhysicalDeviceType}, DeviceCreateInfo, QueueCreateInfo, Device}, VulkanError
 };
 use winit::{event_loop::EventLoop, window::Window};
 
-type MyVulkanError = Box<dyn Error>;
+type VulkanApiError = Box<dyn Error>;
 
 fn init_vulkan_and_window(
     event_loop: &EventLoop<()>,
     window: Arc<Window>,
-) -> Result<(Arc<Instance>, Arc<Surface>), MyVulkanError> {
+) -> Result<(Arc<Instance>, Arc<Surface>), VulkanApiError> {
     let library = VulkanLibrary::new()?;
     let required_extensions = Surface::required_extensions(event_loop);
     let vk_instance = Instance::new(
@@ -29,11 +30,55 @@ fn init_vulkan_and_window(
     Ok((vk_instance, vk_surface))
 }
 
-pub fn init(event_loop: &EventLoop<()>, window: Arc<Window>) -> Result<(), MyVulkanError> {
+fn get_graphics_device(vk_instance: Arc<Instance>, vk_surface: Arc<Surface>) -> Result<(Arc<Device>, Arc<Queue>), VulkanApiError> {
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..Default::default()
+    };
+
+    let (device, mut queues) = vk_instance
+        .enumerate_physical_devices()?
+        .filter(|pysical_device| pysical_device.supported_extensions().contains(&device_extensions))
+        .filter_map(|pysical_device| {
+            pysical_device.queue_family_properties()
+                .iter()
+                .enumerate()
+                .position(|(i, queue_family_properties)| {
+                    queue_family_properties.queue_flags.contains(QueueFlags::GRAPHICS)
+                        && pysical_device.surface_support(i as u32, &vk_surface).unwrap_or(false)
+                })
+                .map(|q| (pysical_device, q as u32))
+        })
+        .min_by_key(|(pysical_device, _)| match pysical_device.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            _ => 4,
+        })
+        .ok_or(VulkanError::DeviceLost) // FIX: Use my own error because this is wrong
+        .map(|(physical_device, queue_family_index)| {
+            Device::new(
+                physical_device.clone(),
+                DeviceCreateInfo {
+                    queue_create_infos: vec![QueueCreateInfo {
+                        queue_family_index,
+                        ..Default::default()
+                    }],
+                    ..Default::default()
+                }
+            )
+        })??;
+
+    Ok((device, queues.next().unwrap()))
+}
+
+pub fn init(event_loop: &EventLoop<()>, window: Arc<Window>) -> Result<(), VulkanApiError> {
     // init vulkan and window
     let (vk_instance, vk_surface) = init_vulkan_and_window(event_loop, window.clone())?;
 
-    // setup graphics device
+    // get graphics device
+    let (device, queue) = get_graphics_device(vk_instance, vk_surface)?;
 
     // create swapchain
 
