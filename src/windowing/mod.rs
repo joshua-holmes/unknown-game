5082ef36cb46d7b9bd92d0a527a10d3bb7564f25
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-01-21 20:57:26 -0800

    Rename dir

--- src/windowing/mod.rs
+++ src/windowing/mod.rs
@@ -1,14 +1,12 @@
-use std::sync::Arc;
+use std::{sync::Arc, error::Error};
 
 use winit::{event_loop::EventLoop, window::Window};
 
 mod vulkan;
 
-pub enum MyWindowError {
+type WindowingError = Box<dyn Error>;
 
-}
-
-pub fn init_window() {
+pub fn init_window() -> Result<(), WindowingError> {
     let event_loop = EventLoop::new();
     let window = {
         let window = Window::new(&event_loop)?;
@@ -16,4 +14,6 @@ pub fn init_window() {
     };
 
     vulkan::init(&event_loop, window.clone());
+
+    Ok(())
 }
