9d819f1adf7f4de124491664b8d4deb4c07e08b7
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-02-24 12:05:23 -0800

    Refactor vel and pos setting for Dot

--- src/game/dot.rs
+++ src/game/dot.rs
@@ -20,25 +20,30 @@ impl Dot {
     }
 
     pub fn set_next_frame(&mut self, resolution: &Resolution, delta_time: &Duration) {
-        let props = self.material.properties();
+        self.set_position(resolution, delta_time);
+        self.set_velocity(resolution, delta_time);
+    }
 
+    fn set_position(&mut self, resolution: &Resolution, delta_time: &Duration) {
         let unclamped_position = self.velocity * delta_time.as_secs_f64() + self.position;
         let new_position = unclamped_position.clamp(
             Some(Vec2::new(0., 0.)),
             Some(Vec2::new((resolution.width - 1) as f64, (resolution.height - 1) as f64))
         );
         self.position = new_position;
+    }
 
-        let floor_collision = new_position.y == (resolution.height - 1) as f64 && self.velocity.y >= 0.;
-        let ceil_collision = new_position.y == 0. && self.velocity.y < 0.;
+    fn set_velocity(&mut self, resolution: &Resolution, delta_time: &Duration) {
+        let floor_collision = self.position.y == (resolution.height - 1) as f64 && self.velocity.y >= 0.;
+        let ceil_collision = self.position.y == 0. && self.velocity.y < 0.;
         self.velocity.y = if floor_collision || ceil_collision {
             0.
         } else {
             self.velocity.y + GRAVITY * delta_time.as_secs_f64()
         };
 
-        let right_wall_collision = new_position.x == (resolution.width - 1) as f64 && self.velocity.x > 0.;
-        let left_wall_collision = new_position.x == 0. && self.velocity.x < 0.;
+        let right_wall_collision = self.position.x == (resolution.width - 1) as f64 && self.velocity.x > 0.;
+        let left_wall_collision = self.position.x == 0. && self.velocity.x < 0.;
         self.velocity.x = if right_wall_collision || left_wall_collision {
             0.
         } else {
